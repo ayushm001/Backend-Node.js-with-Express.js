23ad9079c04e95dd837b3e81e46b9581c6ffc5d2
Ayushman Rout <ayushmanroutt01@gmail.com>
2025-01-05 22:37:11 +0530

    status code

--- week-2/main.js
+++ week-2/main.js
@@ -2,13 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(bodyParser.json( ))
 
-function middleware1(req, res, next) {
-    console.log("from insider the middlwa re" + req.headers.counter);
-    next();    // this  helps to transmit the request to next function  
 
-}
-app.use(middleware1);  // here whenever we try 
+// function middleware1(req, res, next) {
+//     console.log("from insider the middlwa re" + req.headers.counter);
+//     next();    // this  helps to transmit the request to next function  
+
+// }
+// app.use(middleware1);  // here whenever we try 
 
 
 function calculatesum (counter) {
@@ -16,29 +18,33 @@ function calculatesum (counter) {
     for (var i=0 ; i <= counter; i++) {
         sum = sum+i ;
     }
-    return sum;
+    return sum; 
 }                            // operation process
 
-// function handleFirstRequest(req,res) {
-//     console.log(req.params.username1);
+function handleFirstRequest(req,res) {
 
-//     var counter = req.query.counter;
-//     var calculatedsum = calculatesum(counter);
-
-//     var answer = `the sum is ${calculatedsum}`
-//     res.send(answer);
-// }
-// app.get('/:username1' , handleFirstRequest)
-
-function handleFirstRequest (req, res) {
-    console.log(req.headers);
-    var counter = req.headers.counter;           // another way to send data (header)
-    var calculatedsum = calculatesum(counter);  // allows user to pass values
-    
+    var counter = req.query.counter;
+    console.log(req.query.counter2);
+    console.log(req.query.counter3);
+    var calculatedsum = calculatesum(counter);
 
     var answer = `the sum is ${calculatedsum}`
-    res.send(answer)
-}                           // Exposite in Localhost
+    // res.send(answer);
+    res.status(401).send(answer):
+}
+app.get('/handleSum' , handleFirstRequest)
+
+
+
+// function handleFirstRequest (req, res) {
+//     console.log(req.headers);
+//     var counter = req.headers.counter;           // another way to send data (header)
+//     var calculatedsum = calculatesum(counter);  // allows user to pass values
+    
+
+//     var answer = `the sum is ${calculatedsum}`
+//     res.send(answer)
+// }                           // Exposite in Localhost
 
 
 //app.get('/handleSum', handleFirstRequest)    //  '/handleSum' - is a route which can callback the  handleFirstRequest function
