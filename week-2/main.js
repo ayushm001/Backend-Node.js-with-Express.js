a725b91d5344461613d29e48a60599ed4f101bfa
Ayushman Rout <ayushmanroutt01@gmail.com>
2025-01-09 23:52:40 +0530

    Statuscode responses

--- week-2/main.js
+++ week-2/main.js
@@ -12,8 +12,9 @@ const port = 3000
 // }
 // app.use(Middleware1);
 
-app.use(bodyParser.json())  // extr  act the body                         
+app.use(bodyParser.json())  // extr    act the body                         
 
+//
 function calculatesum (counter) {
     var sum =0;
     for (var i=0 ; i <= counter; i++) {
@@ -22,17 +23,53 @@ function calculatesum (counter) {
     return sum; 
 }                            // operation process
 
+//
+function calculateMulti(counter) {
+    var answer = 1;
+    for (var i = 1; i<= counter; i++) {
+        answer= answer * i;
+    }
+    return answer;
+}
 
 
 
-function handleFirstRequest (req, res) {
-    console.log(req.query);
-    console.log(req.body);   // In this case  it prints Undefined 
-    var counter = req.body.counter;           // another way to send data (header)
-    var calculatedsum = calculatesum(counter);  // allows user to pass values
+
+ function handleFirstRequest (req, res) {
+
+    var counter = req.body.counter;
+
+    var calculatedSum = calculatesum(counter);
+    var calculateMulti = calculateMulti(counter);
+
+    var answerObject = {
+        sum : calculatedSum,
+        mul : calculateMulti, 
+    }  
+
+    res.status(200).send(answerObject);
+
+
+
+
+
+
+    // console.log(req.query);
+    // console.log(req.body);   // In this case  it prints Undefined 
+    // var counter = req.body.counter;           // another way to send data (header)
+
+    // if (counter < 1000) {
+    //     var calculatedsum = calculatesum(counter);
+    //     var answer = `the sum is ${calculatedsum}`
+    //     res.send(answer)
+
+    // }
+    // else {
+    //     res.status(411).send(`You have very big number`)
+    // }
+    // var calculatedsum = calculatesum(counter);  // allows user to pass values
      
-    var answer = `the sum is ${calculatedsum}`
-    res.send(answer)
+
 }                           // Exposite in Localhost
 
 
